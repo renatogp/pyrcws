71de013aef0c2b1f70689ae407ae09dc6529ca15
Renato Garcia Pedigoni <rpedigoni@Renato-Pedigonis-iMac.local>
2010-10-27 14:40:59 -0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(
+    name='pyrcws',
+    version=version,
+    description="pyrcws",
+    long_description="",
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='redecard',
+    author='Renato Pedigoni',
+    author_email='renatopedigoni@gmail.com',
+    url='http://github.com/rpedigoni/pyrcws',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['suds'],
+)
